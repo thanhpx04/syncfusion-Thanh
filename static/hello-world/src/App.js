5bbc4dc56937f6db2f1f72f5b1da31b6eb1ac9a2
thanhpx04 <thanhpx04@gmail.com>
2022-12-23 17:02:45 +0700

    sync

--- static/hello-world/src/App.js
+++ static/hello-world/src/App.js
@@ -1,33 +1,57 @@
 import React, { useEffect, useState } from 'react';
 import { TreeGridComponent, ColumnsDirective, ColumnDirective } from "@syncfusion/ej2-react-treegrid";
 import { ButtonComponent } from '@syncfusion/ej2-react-buttons';
-import issueData, { findChildByJql } from "./data/fetchData";
+import issueData, { findChildByJql, loadChild } from "./data/fetchData";
 import './App.css';
 
 function App() {
   const [dataSource, setDataSource] = useState([]);
+  let projects = "TEST";
+  let issueLinkType = {
+    id: "10008",
+    name: "Track/Contributes",
+    inward: "Contributes To",
+    outward: "Tracks"
+  }
   let treegridIssue;
 
   const handleClickSearch = async () => {
-    treegridIssue && treegridIssue.showSpinner(); // show the spinner  
-    let issueLinkType = {
-      id: "10008",
-      name: "Track/Contributes",
-      inward: "Contributes To",
-      outward: "Tracks"
-    }
-    let value = await issueData("TEST", issueLinkType, "");
-    console.log("get value");
+    treegridIssue && treegridIssue.showSpinner(); // show the spinner
+    let value = await issueData(projects, issueLinkType, "");
     treegridIssue && treegridIssue.hideSpinner(); // hide the spinner  
     setDataSource(value);
   };
   
   const onExpandingChange = (event) => {
-    console.log("expanding");
+    console.log("Expanding");
+    if(event.data){
+      treegridIssue && treegridIssue.showSpinner(); // show the spinner 
+      let issueParent = event.data;
+      Promise.all(
+        issueParent.childIssues.map(async (child) => {
+          let childOfChild = await findChildByJql(
+            projects,
+            issueLinkType,
+            child
+          );
+          await loadChild(dataSource, child.key, childOfChild);
+        })
+      ).then(() => {
+        treegridIssue && treegridIssue.hideSpinner(); // hide the spinner 
+        setDataSource(dataSource);
+        console.log(dataSource);
+        // setExpanded(
+        //   event.value
+        //     ? expanded.filter((id) => id !== event.dataItem.id)
+        //     : [...expanded, event.dataItem.id]
+        // );
+      }); 
+
+    }
   }
   
   const onExpandedChange = (event) => {
-    console.log("expanded");
+    console.log("Expanded");
   };
 
   return (
@@ -42,7 +66,7 @@ function App() {
             dataSource={dataSource}
             treeColumnIndex={0}
             enableCollapseAll="true"
-            childMapping="issues"
+            childMapping="childIssues"
             expanding={onExpandingChange}
             expanded={onExpandedChange}
           >
--- static/hello-world/src/data/fetchData.js
+++ static/hello-world/src/data/fetchData.js
@@ -3,7 +3,7 @@ import { requestJira } from "@forge/bridge"
 const data = async (projects, linkType, issueKey) => {
     // let listProject = projects.map(element => JSON.stringify(element.key))
     // const params = issueKey === "" ? `project in (${listProject}) AND (filter != ${linkType.id})` : `project in (${listProject}) AND (filter != ${linkType.id}) AND issue =${issueKey}`;
-    const params = issueKey === "" ? `project = ${projects} AND (filter != ${linkType.id})` : `project = ${projects} AND (filter != ${linkType.id}) AND issue =${issueKey}`;
+    const params = issueKey === "" ? `project = ${projects} AND (filter != "${linkType.id}")` : `project = ${projects} AND (filter != "${linkType.id}") AND issue =${issueKey}`;
     const response = await requestJira(`/rest/api/2/search?jql=${params}`);
     return await response.json();
 };
@@ -16,7 +16,7 @@ const issueData = async (projects, linkType, issueKey) => {
             error: result.errorMessages
         };
     }
-    let issues = [];
+    let childIssues = [];
     await Promise.all(result.issues.map(async (element) => {
         let item = {
             id: element.id,
@@ -31,16 +31,16 @@ const issueData = async (projects, linkType, issueKey) => {
         }
 
         let children = await findChildByJql(projects, linkType, item);
-        item.issues = children;
-        issues.push(item)
+        item.childIssues = children;
+        childIssues.push(item)
     }))
-    return issues.sort((a, b) => b.id - a.id);
+    return childIssues.sort((a, b) => b.id - a.id);
 }
 
 export const findChildByJql = async (projects, linkType, issue) => {
     // let listProject = projects.map(element => JSON.stringify(element.key))
     // let jqlFindChildByID = `project in (${listProject}) and issue in linkedIssues("${issue.key}", ${linkType.outward})`
-    let jqlFindChildByID = `project = ${projects} and issue in linkedIssues("${issue.key}", ${linkType.outward})`
+    let jqlFindChildByID = `project = ${projects} and issue in linkedIssues("${issue.key}", "${linkType.outward}")`
     let url = `/rest/api/2/search?jql=${jqlFindChildByID}`
     const response = await requestJira(url);
     const data = await response.json();
@@ -61,6 +61,19 @@ export const findChildByJql = async (projects, linkType, issue) => {
     })
     return listChildren;
 }
+
+export const loadChild = async (source, parentKey, childIssues) => {
+    source.forEach((element) => {
+        if (element.key === parentKey) {
+            element.childIssues = childIssues;
+            return source;
+        }
+        if (element.childIssues !== undefined) {
+            loadChild(element.childIssues, parentKey, childIssues);
+        }
+    });
+};
+
 const getIssueLinks = async (issueKey) => {
     const response = await requestJira(`/rest/api/3/issue/${issueKey}?fields=issuelinks`);
     const data = await response.json()
